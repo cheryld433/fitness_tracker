88a4c0d524370cc4fecb39fce47594b52f4b8418
Cheryl Daniels <cheryld433@gmail.com>
2020-07-06 23:21:35 -0400

    added index file and constructed mongoose schema

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,58 @@
 // may need another file!!!! index.js
+//requires mongoose
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const workoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: new Date
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+                weight: {
+                    type: Number,
+                    default: 0
+                },
+                sets: {
+                    type: Number,
+                    default: 0
+                },
+                reps: {
+                    tyoe: Number,
+                    default: 0
+                },
+                duration: {
+                    type: Number,
+                    default: 0,
+                    required: "Enter an exercise duration in minutes"
+                }
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        }
+    }
+);
+workoutSchema.virtual("totalDuration").get(function (){
+    return this.exercises.reduce((total,exercise) => {
+        return total + exercise.duration;
+    }, 0);
+})
+
+const workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
