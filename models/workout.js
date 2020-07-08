4e3da185d3a731a9d3e28f9e59d1c5eabbc1ec2c
Cheryl Daniels <cheryld433@gmail.com>
2020-07-08 16:00:59 -0400

    modified files

--- models/workout.js
+++ models/workout.js
@@ -51,6 +51,6 @@ workoutSchema.virtual("totalDuration").get(function (){
     }, 0);
 })
 
-const workout = mongoose.model("Workout", workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
